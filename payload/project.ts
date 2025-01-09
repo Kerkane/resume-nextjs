2111371b58610c57823cfb0bbc5aa944e12b3141
ogrim <ogrim123@gmail.com>
2025-01-10 02:55:40 +0900

    update

--- payload/project.ts
+++ payload/project.ts
@@ -4,38 +4,64 @@ const project: IProject.Payload = {
   disable: false,
   list: [
     {
-      title: 'Linux Kernel Optimization Project',
-      startedAt: '2020-06',
-      where: 'Linux Foundation',
+      title: 'HTML Lexer and Parser in Python',
+      startedAt: '2021-10',
+      endAt: '2021-11',
+      where: 'Team Project',
       descriptions: [
         {
           content:
-            'Initiated and lead a collaborative project aimed at optimizing the Linux Kernel for various hardware architectures.',
-        },
+            'Python을 이용해 입력된 문자열을 어휘 단위로 쪼개 의미별로 토큰화하는 Lexer 구현'
+	},
         {
           content:
-            'Achieved significant improvements in system performance and resource management.',
-          weight: 'MEDIUM',
-          descriptions: [
-            { content: '30% improvement in system resource efficiency' },
-            { content: 'Job Scheduler Refactor and Optimization' },
-          ],
+            '토큰화된 문자열에 대해 Context-free Grammar를 기준으로 구문적 의미를 분석하는 Parser 구현 하며 프로그래밍 언어의 문법과 구조를 익힘'
         },
       ],
     },
     {
-      title: 'Global Linux Bootcamp',
-      startedAt: '2017-05',
-      endedAt: '2019-12',
-      where: 'Open Source Community',
+      title: 'Face Tracking with 2D Discrete Fourier Transform',
+      startedAt: '2021-05',
+      endAt: '2021-06',
+      where: 'Personal Project',
       descriptions: [
         {
           content:
-            'Organized and conducted training bootcamps focusing on Linux system engineering skills.',
-        },
+            '신호 처리와 신경망 기법에 사용되는 Convoltion을 구현하면서 Numpy의 텐서 계산을 익힘'},
         {
           content:
-            'Successfully trained over 5,000 participants in system administration and security. ',
+            'Convolution을 통해 구현한 2D Fourier Transform으로 이미지에서 얼굴만을 인식하는 Face Tracking 개발'},
+      ],
+    },
+    {
+      title: 'Implementation of Cyclic Redundancy Check in Verilog',
+      startedAt: '2021-05',
+      endAt: '2021-06',
+      where: 'Team Project',
+      descriptions: [
+        {
+          content:
+            '순환 중복 검사(CRC)를 수행하는 논리 회로 모델링 디자인'
+       	},
+        {
+          content:
+            '모델 디자인을 기반으로 베릴로그를 이용한 논리 회로 구현'
+        },
+      ],
+    },
+    {
+      title: 'Optimal purchase list with greedy algorithm',
+      startedAt: '2019-10',
+      endedAt: '2019-11',
+      where: 'Personal Project',
+      descriptions: [
+        {
+          content:
+            'Python을 이용한 Steam에서의 데이터 크롤링 구현'
+       	},
+        {
+          content:
+            '크롤링 데이터를 사용한 greedy algorithm 구현'
         },
       ],
     },
